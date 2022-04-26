806e72195a093cda80b24769dc7d715574662bbe
Saurabh <saurabh.saini@te.com>
2022-04-26 12:41:56 +0530

    leave count methord change to post

--- modules/leave/router.js
+++ modules/leave/router.js
@@ -16,6 +16,6 @@ router.get('/leave_request/:assigned_to_id',auth(1),leaveController.leaveRequest
 router.put('/leave_approve_reject/:leave_id',auth(1),leaveController.leaveApproveReject);
 
 // this web service is use to count the total leave taken by the user in a year.
-router.get('/leave_count',auth(1),leaveController.leaveCount);
+router.post('/leave_count',auth(1),leaveController.leaveCount);
 
 module.exports = router;
