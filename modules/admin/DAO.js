52b04db00cc791b0926d53f9efc5db649fbf8743
Saurabh <saurabhsaini38@gmail.com>
2023-02-07 22:52:46 +0530

    update code

--- modules/admin/DAO.js
+++ modules/admin/DAO.js
@@ -114,7 +114,7 @@ async function adminTotal(user_id,date) {
         let absentCount = userCout - odCount - presentToday - onLeaveToday;
         console.log("absent today ",absentCount);
 
-        console.log("list of total users ",userListTotal);
+        console.log("list of total users ",userListTotal.user.dataValues[0]);
 
 
         // for(let i = 0; i< userListTotal.length; i++)
